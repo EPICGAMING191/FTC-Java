267fd011389af203e7a37d7e9c2590b4184be053
EPICGAMING191 <112524668+EPICGAMING191@users.noreply.github.com>
2023-02-03 09:02:01 -0500

    Update motortest.java

--- Motors/motortest.java
+++ Motors/motortest.java
@@ -1,4 +1,6 @@
 package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.hardware.DcMotor;
 private DCMotor motor;
 @TeleOp
