063e021306f714188c1b63d88e7eda597408da83
EPICGAMING191 <112524668+EPICGAMING191@users.noreply.github.com>
2023-02-03 16:18:25 -0500

    Update servotest.java

--- Servos/servotest.java
+++ Servos/servotest.java
@@ -1,5 +1,20 @@
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+private Servo servo;
 Public class servotest extends OpMode(){
+  public void init(){
 servo = hardwareMap.get(Servo.class, "servo");
 servo.setDirection(Servo.Direction.REVERSE);
 servo.scaleRange(0.5, 1.0); 
+  }
+  public void loop(){
+    If (servo.getPosition = 0){
+    servo.setPosition(1);
+    }
+     If (servo.getPosition = 1){
+    servo.setPosition(0);
+    }
+  }
 }
+
