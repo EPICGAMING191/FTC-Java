c657548f4752185d6b52838ae4023a87c8cd6567
EPICGAMING191 <112524668+EPICGAMING191@users.noreply.github.com>
2023-02-03 08:42:32 -0500

    Update mecdrive.java

--- Drive/mecdrive.java
+++ Drive/mecdrive.java
@@ -1,1 +1,11 @@
+//Work IN PROGRESS DO NOT USE IT WILL GIVE ERRORS
+public class MecDrive extends OpMode{
+  public void init(){
+  }
+  public void loop(){
+    double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+    double x = -gamepad1.left_stick_x; // Counteract imperfect strafing
+    double rx = -gamepad1.right_stick_x; //This is reversed for our turning
 
+  }
+}
