7d4d1817b898ce81e51677157e1acc3a69cc1e9d
EPICGAMING191 <112524668+EPICGAMING191@users.noreply.github.com>
2023-02-07 08:12:48 -0500

    Update grippertest.java

--- Servos/grippertest.java
+++ Servos/grippertest.java
@@ -1,3 +1,19 @@
-
-public class grippertest extends OpMode(){
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.hardwareMap;
+public class grippertest extends OpMode{
+    private Servo servo1;
+    public void init() {
+    servo1=hardwareMap.Get(Servo.class,"servo");
+    }
+    public void loop() {
+        servo.setPosition(0);
+    if (servo.getposition()==0){
+        servo1.setPosition(1);
+    }
+    else if(servo.getposition()==1){
+        servo1.setPosition(0);
+    }
+}
 }
