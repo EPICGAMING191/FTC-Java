181a7daf4d282c6827dcf312f226418cfd5143f0
EPICGAMING191 <112524668+EPICGAMING191@users.noreply.github.com>
2023-02-21 16:49:44 -0500

    Create ColorSensorIdea.java

---
+++ 2022-2023/Sensors/ColorSensorIdea.java
@@ -0,0 +1,29 @@
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+@TeleOp
+public class EliColorSensorIdea extends OpMode {
+    private double red;
+    private double green;
+    private double blue;
+    private ColorSensor color;
+    @Override
+    public void init() {
+       color=hardwareMap.get(ColorSensor.class,"color");
+    }
+
+    @Override
+    public void loop() {
+        getColorRGB();
+        telemetry.addData("Red:",red);
+        telemetry.addData("Green:",green);
+        telemetry.addData("Blue:",blue);
+
+    }
+    public void getColorRGB(){
+        red=color.red();
+        green=color.green();
+        blue=color.blue();
+    }
+}
