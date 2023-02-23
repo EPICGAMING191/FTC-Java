6f4529e17068f01a02222de439ac113fe6af063a
EPICGAMING191 <112524668+EPICGAMING191@users.noreply.github.com>
2023-02-23 16:57:21 -0500

    Add files via upload

---
+++ ChatGPT/GPTDrive.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.Drive;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+public class GPTDrive extends OpMode {
+    private DcMotor frontLeftMotor;
+    private DcMotor frontRightMotor;
+    private DcMotor backLeftMotor;
+    private DcMotor backRightMotor;
+    private Gamepad gamepad;
+
+    @Override
+    public void init() {
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "front_left_motor");
+        frontRightMotor = hardwareMap.get(DcMotor.class, "front_right_motor");
+        backLeftMotor = hardwareMap.get(DcMotor.class, "back_left_motor");
+        backRightMotor = hardwareMap.get(DcMotor.class, "back_right_motor");
+        gamepad = gamepad1;
+    }
+
+    @Override
+    public void loop() {
+        double drive = -gamepad.left_stick_y;
+        double strafe = gamepad.left_stick_x;
+        double rotate = gamepad.right_stick_x;
+
+        double frontLeftPower = drive + strafe + rotate;
+        double frontRightPower = drive - strafe - rotate;
+        double backLeftPower = drive - strafe + rotate;
+        double backRightPower = drive + strafe - rotate;
+
+        frontLeftMotor.setPower(frontLeftPower);
+        frontRightMotor.setPower(frontRightPower);
+        backLeftMotor.setPower(backLeftPower);
+        backRightMotor.setPower(backRightPower);
+    }
+}
