8738a4e3b13e61104da4d7797205c5750fef7dd5
EPICGAMING191 <112524668+EPICGAMING191@users.noreply.github.com>
2023-02-23 20:03:10 -0500

    Create DriveTestBotv2.java

---
+++ 2022-2023/Drive/DriveTestBotv2.java
@@ -0,0 +1,105 @@
+//EDIT PACKAGE NAME BELOW NOTHING ELSE
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+@TeleOp
+public class DriveTestBot extends OpMode {
+    private DcMotor frontLeftMotor;
+    private double gripperState;
+    private DcMotor frontRightMotor;
+    private DcMotor backLeftMotor;
+    private DcMotor backRightMotor;
+    private double y;
+    private double x;
+    private double rx;
+    private Servo grippers;
+    private Servo wrist;
+    ElapsedTime MainTimer = new ElapsedTime();
+    ElapsedTime CycleTimer = new ElapsedTime();
+    public void init() {
+        gamepad1.rumbleBlips(3);
+        telemetry.addData("Gamepad ID:",gamepad1.getGamepadId());
+        MainTimer.reset();
+        CycleTimer.reset();
+        grippers = hardwareMap.get(Servo.class,"grippers");
+        wrist = hardwareMap.get(Servo.class,"wrist");
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "FL");
+        frontRightMotor = hardwareMap.get(DcMotor.class, "FR");
+        backLeftMotor = hardwareMap.get(DcMotor.class, "BL");
+        backRightMotor = hardwareMap.get(DcMotor.class, "BR");
+
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+
+    public void loop(){
+        double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+        double x = gamepad1.left_stick_x; // Counteract imperfect strafing
+        double rx = gamepad1.right_stick_x; //This is reversed for our turning
+        drive();
+        WorkGrippers();
+	OperateWrist()
+        GetTime();
+    }
+
+
+    public void drive(){
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (-y + x + rx) / denominator;
+        double backLeftPower = (-y - x + rx) / denominator;
+        double frontRightPower = (y + x + rx) / denominator;
+        double backRightPower = (y - x + rx) / denominator;
+
+        frontLeftMotor.setPower(frontLeftPower);
+        backLeftMotor.setPower(backLeftPower);
+        frontRightMotor.setPower(frontRightPower);
+        backRightMotor.setPower(backRightPower);
+    }
+    public void WorkGrippers(){
+        if (gamepad1.x){
+            gripperstate=0;
+            grippers.setPosition(0.45);
+            telemetry.addData("Position","Open");
+            logCycle();    
+            }
+        if (gamepad1.b) {
+            gripperState=0;
+            grippers.setPosition(0.97);
+            telemetry.addData("Position","Closed");
+            }
+        }
+    
+    public void GetTime(){
+        telemetry.addData("Time:",MainTimer.time());
+        telemetry.addData("Time since last delivery:",CycleTimer.time());
+    }
+    public void logCycle(){
+        double lastCycle=CycleTimer.time();
+        telemetry.addData("Previous Cycle:",lastCycle);
+    }
+    public void OperateWrist(){
+        if (gamepad1.y){
+	  wrist.SetPosition(0.25);
+	}
+ 	if (gamepad1.a){
+	  wrist.SetPosition(0.5);
+	}
+}
+}
+
