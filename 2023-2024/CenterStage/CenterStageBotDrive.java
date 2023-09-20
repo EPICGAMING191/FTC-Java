4ac3f0ff2d96a6f09be0dd4c334a500f2d26ab3d
EPICGAMING191 <112524668+EPICGAMING191@users.noreply.github.com>
2023-09-20 08:34:47 -0400

    Update CenterStageBotDrive.java

--- 2023-2024/CenterStage/CenterStageBotDrive.java
+++ 2023-2024/CenterStage/CenterStageBotDrive.java
@@ -1,30 +1,39 @@
-//Not tested yet
 package org.firstinspires.ftc.teamcode;
-public class MecDrive {
- private DcMotor frontLeftMotor;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class MecDrive extends OpMode {
+    private DcMotor frontLeftMotor;
     private DcMotor frontRightMotor;
     private DcMotor backLeftMotor;
     private DcMotor backRightMotor;
 
-    public void init(HardwareMap hwMap) {
-      gamepad1.rumbleBlips(3);
-      telemtry.AddData("Gamepad ID:",gamepad1.getID)
-        frontLeftMotor = hwMap.get(DcMotor.class, "FL");
-        frontRightMotor = hwMap.get(DcMotor.class, "FR");
-        backLeftMotor = hwMap.get(DcMotor.class, "BL");
-        backRightMotor = hwMap.get(DcMotor.class, "BR");
+
+    @Override
+    public void init() {
+        gamepad1.rumbleBlips(3);
+        telemetry.addData("Gamepad ID:",gamepad1.getGamepadId());
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "FL");
+        frontRightMotor = hardwareMap.get(DcMotor.class, "FR");
+        backLeftMotor = hardwareMap.get(DcMotor.class, "BL");
+        backRightMotor = hardwareMap.get(DcMotor.class, "BR");
 
         frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
-  public void loop(){
-    double y = -gamepad1.left_stick_y; // Remember, this is reversed!
-    double x = gamepad1.left_stick_x; // Counteract imperfect strafing
-    double rx = gamepad1.right_stick_x; //This is reversed for our turning
-    drive{x,y,rx};
- }
+
+    public void loop(){
+        double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+        double x = -gamepad1.left_stick_x; // Counteract imperfect strafing
+        double rx = -gamepad1.right_stick_x; //This is reversed for our turning
+        drive(y,x,rx);
+    }
+
+
     public void drive(double y, double x, double rx){
         double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
         double frontLeftPower = (-y + x + rx) / denominator;
@@ -38,4 +47,3 @@ public class MecDrive {
         backRightMotor.setPower(backRightPower);
     }
 }
-
