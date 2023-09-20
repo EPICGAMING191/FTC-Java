111a07af876fde1cf24cc7e9fddc1e99bd4601f1
EPICGAMING191 <112524668+EPICGAMING191@users.noreply.github.com>
2023-09-20 08:51:29 -0400

    Update CenterStageBotDrive.java

--- 2023-2024/CenterStage/CenterStageBotDrive.java
+++ 2023-2024/CenterStage/CenterStageBotDrive.java
@@ -28,7 +28,7 @@ public class MecDrive extends OpMode {
 
     public void loop(){
         double y = -gamepad1.left_stick_y; // Remember, this is reversed!
-        double x = -gamepad1.left_stick_x; // Counteract imperfect strafing
+        double x = gamepad1.left_stick_x; // Counteract imperfect strafing
         double rx = -gamepad1.right_stick_x; //This is reversed for our turning
         drive(y,x,rx);
     }
